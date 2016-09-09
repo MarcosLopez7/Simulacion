49b0639383337721a2c1fba492fc8721d2eb5b35
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-09-08 21:34:03 -0500

    Tal vez ya este la simulacion

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 
 #define N 20000
 #define M 5
@@ -13,30 +14,52 @@ typedef struct {
     bool reparacion;
 } Componentes;
 
-void simular(int **);
+void simular1(float **);
+void simular2(float **);
 
 int main() {
 
     ifstream archivo("Tabla1.txt");
-    int **tabla = (int **) malloc(N * sizeof(int *));
+
+    float **tabla = (float **) malloc(N * sizeof(float *));
 
     for (int i = 0; i < N; ++i) {
-        tabla[i] = (int *) malloc(M * sizeof(int));
+        tabla[i] = (float *) malloc(M * sizeof(float));
     }
 
     int i = 0;
+    int k;
+    string linea, temp;
 
-    while (archivo >> tabla[i][0] >> tabla[i][1] >> tabla[i][2] >> tabla[i][3] >> tabla[i][4]) {
-        i++;
+    if (archivo.is_open()) {
+        while ( getline(archivo, linea) ) {
+            temp = "";
+            k = 0;
+            for (int j = 0; j < linea.length(); ++j) {
+                if (linea.at(j) != '\t'){
+                    temp += linea.at(j);
+                } else if(temp != ""){
+                    tabla[i][k] = atof(temp.c_str());
+                    k++;
+                    temp = "";
+                }
+            }
+
+            tabla[i][k] = atof(temp.c_str());
+
+            i++;
+        }
+
+        archivo.close();
+        simular1(tabla);
+        simular2(tabla);
     }
-
-    simular(tabla);
-
+    archivo.close();
     free(tabla);
     return 0;
 }
 
-void simular(int **tabla) {
+void simular1(float **tabla) {
 
     Componentes comp[M - 1];
 
@@ -46,7 +69,7 @@ void simular(int **tabla) {
         comp[i].reparacion = false;
     }
 
-    int **t = tabla;
+    float **t = tabla;
     int gasto = 0;
 
     for (int i = 0; i < N; ++i) {
@@ -54,11 +77,11 @@ void simular(int **tabla) {
         bool falla = false;
 
         for (int j = 0; j < M - 1; ++j) {
-            if ((*t)[j + 1] >= comp[j].horas) {
+            if ((*t)[j + 1] <= comp[j].horas) {
                 falla = true;
-                cout <<  "El componenete " << comp[j].id << " presenta falla en la hora " << i <<
+         /*       cout <<  "El componenete " << comp[j].id << " presenta falla en la hora " << i <<
                         ", se restablecera las horas de todos los componentes dado que se van a remplazar\n";
-                break;
+         */      break;
             }
         }
 
@@ -67,7 +90,7 @@ void simular(int **tabla) {
                 comp[j].horas++;
             }
 
-            cout << "No se presento ninguna falla en la hora " << i << endl;
+         //   cout << "No se presento ninguna falla en la hora " << i << endl;
         } else {
             gasto += 1000;
             for (int j = 0; j < M - 1; ++j) {
@@ -82,4 +105,54 @@ void simular(int **tabla) {
     cout << "\nEl costo total por la politica de rempalazar todos los "
                     "componentes por uno fallido, generaria un costo de:" << gasto << endl << endl;
 
+}
+
+void simular2(float **tabla){
+
+    Componentes comp[M - 1];
+
+    for (int i = 0; i < M - 1; ++i) {
+        comp[i].id = i + 1;
+        comp[i].horas = 0;
+        comp[i].reparacion = false;
+    }
+
+    float **t = tabla;
+    int gasto = 0;
+    int fallas[M - 1] = {0, 0, 0, 0};
+
+    for (int i = 0; i < N; ++i) {
+
+        bool falla = false;
+
+        for (int j = 0; j < M - 1; ++j) {
+            if (t[fallas[j]][j + 1] <= comp[j].horas) {
+                falla = true;
+  /*              cout <<  "El componenete " << comp[j].id << " presenta falla en la hora " << i <<
+                ", se restablecera las horas de todos los componentes dado que se van a remplazar\n";
+    */            comp[j].reparacion = true;
+            }
+        }
+
+        if (!falla) {
+            for (int j = 0; j < M - 1; ++j) {
+                comp[j].horas++;
+            }
+
+      //      cout << "No se presento ninguna falla en la hora " << i << endl;
+        } else {
+            gasto += 300;
+            for (int j = 0; j < M - 1; ++j) {
+                if(comp[j].reparacion) {
+                    comp[j].horas = 0;
+                    fallas[j]++;
+                    comp[j].reparacion = false;
+                }
+            }
+            i++;
+        }
+    }
+
+    cout << "\nEl costo total por la politica de rempalazar solo un "
+            "componente fallido, generaria un costo de:" << gasto << endl << endl;
 }
